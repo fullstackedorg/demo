b401e647e9372e7a8d96c51864b9eefcd7bda712
cplepage <lepage.charlesphilippe@gmail.com>
2024-07-22 11:46:16 -0400

    v0.8.0

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ webcontainerInstance.mount({
                     "start": "fullstacked"
                 },
                 dependencies: {
-                    "@fullstacked/editor": "0.6.0"
+                    "@fullstacked/editor": "0.8.0"
                 }
             }, null, 2)
         }
