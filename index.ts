b1eea76e12253be3c6c6dc82d1ae9ed6b53fac78
cplepage <lepage.charlesphilippe@gmail.com>
2025-03-03 15:06:19 -0500

    looking good with git package

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import "./webview/counter";
 import "./webview/icons";
 import "./webview/theme";
+import "./webview/links";
+import * as UI from "@fullstacked/ui";
+
+UI.init();
 
 import platform from "platform";
 document.querySelector<HTMLSpanElement>("#platform").innerText = platform;
