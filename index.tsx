0b0d73099656da2ccb90e62231badb5fa0fbe6f4
cplepage <lepage.charlesphilippe@gmail.com>
2026-02-23 01:11:10 -0500

    document title

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
 
+document.title = "Demo"
+
 const container = document.createElement("div");
 document.body.append(container)
 const root = createRoot(container);
